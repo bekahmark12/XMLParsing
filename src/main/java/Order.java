b2d31947940529c5fcbb3d5951606f002d174396
bekahmark12 <rebekahmark12@gmail.com>
2020-11-23 08:58:34 -0700

    added order and orderLine classes

---
+++ src/main/java/Order.java
@@ -0,0 +1,38 @@
+public class Order {
+    private int OrderId;
+    private int CustomerId;
+    private int Total;
+
+    public int getOrderId() {
+        return OrderId;
+    }
+
+    public void setOrderId(int orderId) {
+        OrderId = orderId;
+    }
+
+    public int getCustomerId() {
+        return CustomerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        CustomerId = customerId;
+    }
+
+    public int getTotal() {
+        return Total;
+    }
+
+    public void setTotal(int total) {
+        Total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "OrderId=" + OrderId +
+                ", CustomerId=" + CustomerId +
+                ", Total=" + Total +
+                '}';
+    }
+}
